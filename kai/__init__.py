44f88a65090196125be40d49bb0aea836aca223a
DavAug <daugu@equinor.com>
2024-10-28 14:02:57 +0000

    Finalise notebook

---
+++ kai/__init__.py
@@ -0,0 +1,10 @@
+from ._utils_notebook1 import (
+    perceptron,
+    plot_perceptron,
+    neural_network,
+    plot_training_results,
+    plot_step_activation,
+    plot_sigmoid_activation,
+    plot_results2,
+    plot_four_images
+)
